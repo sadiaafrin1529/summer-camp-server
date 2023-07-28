01a8f8a9b79e47a34539d7d117fcc5acbdebc5f6
sadiaafrin1529 <sadiaafrin1529@gmail.com>
2023-07-28 21:01:39 +0600

    role added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.PAYEMNT_KEY);
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 //midelware
@@ -12,8 +13,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jlk6nyv.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mhchwsf.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,15 +27,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+    // Send a ping to confirm a successful connection
     const usersCollecion = client.db("ArtssCraft").collection("users");
     const courseCollecion = client.db("ArtssCraft").collection("courses");
     const cartColleection= client.db("ArtssCraft").collection("selectcourse");
+    const paymentCollection = client.db("ArtssCraft").collection("payments")
 
 
-    //users
+    
     app.get('/users', async (req, res) => {
       const result = await usersCollecion.find().toArray();
+      res.send(result)
+  })
+    app.get('/users/personalData', async (req, res) => {
+      const email= req.query.email;
+      const result = await usersCollecion.findOne({email:email});
       res.send(result)
   })
 
@@ -78,30 +84,19 @@ async function run() {
       res.send(result)
 
     })
-
-    
-
-
-
-
-
-
+    app.get("/users/role/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollecion.findOne(query);
+      res.send({ isRole: user?.role });
+    });
 
 
-
-
-
-    //courses
+   //   //courses
     app.get('/courses', async (req, res) => {
       const result = await courseCollecion.find({}).toArray()
       res.send(result)
     })
-
-    
-
-
-
-
     app.post('/courses', async (req, res) => {
       const newCourse = req.body;
       const result = await courseCollecion.insertOne(newCourse)
@@ -114,7 +109,7 @@ async function run() {
       const updateDoc = { $set: { status: "approved" } };
   
       const result = await courseCollecion.updateOne(filterData, updateDoc);
-      console.log(result)
+      // console.log(result)
       res.send(result)
     
   });
@@ -146,17 +141,13 @@ async function run() {
   app.get('/courses/myclass/:email', async(req,res)=>{
     const email = req.params.email
     const query = {Email:email}
+    // console.log(query)
     const result = await courseCollecion.find(query).toArray()
     res.send(result);
+    // console.log(result)
+    })
 
-  })
 
-  // app.get('/courses/editCourse/:id',async(req,res)=>{
-  //   const id = req.params.id;
-  //   const query = {_id : new ObjectId(id)}
-  //   const result = await courseCollecion.findOne(query)
-  //   res.send(result)
-  // })
 
   app.get('/dashboard/edit/:id',async(req,res)=>{
     const id = req.params.id;
@@ -165,24 +156,12 @@ async function run() {
     res.send(result)
   })
 
-  // app.put('/courses/:id',async(req,res)=>{
-  //   const id= req.params.id;
-  //   const data= req.body;
-  //   console.log(id,data)
-  //   const query = {_id : new ObjectId(id)}
-  //   const updateDoc ={
-  //     $set:{
-  //       ...data
-  //     }
-  //   }
-  //   const result = await courseCollecion.updateOne(query,data)
-  //   res.send(result)
-  // })
+  
 
   app.put('/courses/:id', async (req, res) => {
     const id = req.params.id;
     const data = req.body;
-    console.log(id, data);
+    // console.log(id, data);
     const query = { _id: new ObjectId(id) };
     const updateDoc = {
       $set: {
@@ -211,23 +190,55 @@ async function run() {
   })
 
   app.get('/addcart/:email',async(req,res)=>{
-    const email= req.params.email;
+    const email= req.params.email
 
-    const query={userEmail: email}
-    const result= await cartColleection.findOne(query)
+    const query={userEmail:email}
+    const result= await cartColleection.find(query).toArray()
     res.send(result)
   })
 
 
+  //Payment
+  app.get('/addcart/payment/:id',async (req,res)=>{
+    const id = req.params.id 
+    console.log(id)
+    const query = {_id:new ObjectId(id)}
+    const result = await cartColleection.findOne(query)
+    res.send(result)
+    // console.log(result)
+  })
+
+  app.post('/payments',async(req,res)=>{
+    const info = req.body
+    const id = info.payment
+    const query = {_id:new ObjectId(id)}
+    const result = await paymentCollection.insertOne(info)
+
+    const deleteResult = await cartColleection.deleteOne(query)
+    res.send({result,deleteResult})
+  })
 
 
+app.get('/payments/:email',async(req,res)=>{
+  const email = req.params.email
+  const query = {email:email}
+  const result = await paymentCollection.find(query).toArray()
+  res.send(result)
+})
 
+app.post("/create-payment-intent", async (req, res) => {
+  const { price } = req.body;
+  const amount = price * 100;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    payment_method_types: ["card"],
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
-
-
-
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -236,6 +247,8 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
+
 
 
 
